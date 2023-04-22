392e7b2cb37bf869234f9e750f1b325b39ad019f
Marta Chaika <121798160+janeseagul@users.noreply.github.com>
2023-04-22 23:48:56 +0300

    download book covers.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from pathvalidate import sanitize_filename
 from bs4 import BeautifulSoup
 import lxml
+from urllib.parse import unquote, urljoin, urlparse
 
 
 def download_txt(url, filename, number, folder='Books/'):
@@ -29,17 +30,35 @@ def download_txt(url, filename, number, folder='Books/'):
     return filepath
 
 
+def download_book_cover(cover_url, folder='Images/'):
+    Path(f'./{folder}').mkdir(exist_ok=True)
+
+    filename = unquote(urlparse(cover_url)).path.split('/')[-1]
+    filename = sanitize_filename(filename)
+    filepath = Path(f'./{folder}/{filename}')
+    response = requests.get(cover_url)
+    response.raise_for_status()
+
+    check_for_redirect(response)
+
+    with open(filepath, 'wb') as file:
+        file.write(response.content)
+
+    return filepath
+
+
 def fetch_book_title(page_url):
     response = requests.get(page_url)
     response.raise_for_status()
 
     soup = BeautifulSoup(response.text, 'lxml')
     title_tag = soup.find('body').find('table').find('td', class_='ow_px_td').find('div').find('h1').text
+    img_tag = soup.find('div', class_='bookimage').find('img')['src']
     title_tag = title_tag.split('   ::   ')
+    book_title = title_tag[0].strip()
+    book_author = title_tag[1].strip()
 
-    title_tag[0].strip()
-    title_tag[1].strip()
-    return title_tag[0]
+    return book_title, img_tag
 
 
 def check_for_redirect(response):
@@ -48,16 +67,22 @@ def check_for_redirect(response):
 
 
 def main():
+    site_link = 'https://tululu.org/'
     for book_id in range(1, 11):
         try:
-            url_pattern = f'https://tululu.org/txt.php?id={book_id}'
-            page_url = f'https://tululu.org/b{book_id}'
-            filename = fetch_book_title(page_url)
+            url_pattern = urljoin(site_link, f'txt.php?=id={book_id}')
+            page_url = urljoin(site_link, f'/b{book_id}')
+            filename, cover_url = fetch_book_title(page_url)
+            cover_url = urljoin(site_link, cover_url)
+            print(cover_url)
 
             download_txt(url_pattern, filename, book_id)
+            download_book_cover(cover_url)
         except requests.exceptions.HTTPError:
             print(f"Can't create book {filename}, it does not exist!")
         except IndexError:
+            print(f"Can't create book {filename}, it does not exist!")
+        except AttributeError:
             print(f"Can't create book {filename}, it does not exist!")
 
 
